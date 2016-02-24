bf2c400c5a9351f8a4eac79e8751f1f8e0f2d8eb
Oliver Stadie <hello@waog.net>
2016-02-24 19:02:55 +0100

    FIXED camera boundaries #2 and ADDED manual zoom #11

---
+++ Assets/Scripts/AdaptSizeToCamera.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AdaptSizeToCamera : MonoBehaviour
+{
+
+	private float PADDING = 0.05f;
+
+	void Update ()
+	{
+
+		transform.localPosition = new Vector3 (Camera.main.transform.localPosition.x, Camera.main.transform.localPosition.y, 0);
+		transform.localScale = new Vector3 (2f * (1f - PADDING) * Camera.main.orthographicSize * Screen.width / Screen.height, 2f * (1f - PADDING) * Camera.main.orthographicSize, transform.localScale.z);
+	}
+}
---
+++ Assets/Scripts/ApplyAsText.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ApplyAsText : MonoBehaviour {
+
+	public void apply(float f) {
+		UnityEngine.UI.Text t = GetComponent<UnityEngine.UI.Text>();
+		t.text = "" + f;
+	}
+}
