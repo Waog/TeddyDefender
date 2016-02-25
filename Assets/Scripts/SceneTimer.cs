763c7ceaaa5f707786a3913b02e859f78edfb681
Oliver Stadie <hello@waog.net>
2016-02-25 19:33:32 +0100

    ADDED #13 timer

---
+++ Assets/Scripts/SceneTimer.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SceneTimer : MonoBehaviour
+{
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		UnityEngine.UI.Text t = GetComponent<UnityEngine.UI.Text> ();
+		t.text = "" + Mathf.FloorToInt(Time.timeSinceLevelLoad);
+	}
+}
