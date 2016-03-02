82dd73b569e76e651393d943382c7bce360235f8
Oliver Stadie <hello@waog.net>
2016-03-02 11:24:16 +0100

    #22 UPDATE: improved game over animation

--- Assets/Scripts/AwakeKidScript.cs
+++ Assets/Scripts/AwakeKidScript.cs
@@ -3,13 +3,14 @@ using System.Collections;
 
 public class AwakeKidScript : MonoBehaviour {
 
-	public GameObject AwakeGameOverWidget;
+	public Animator gameOverAnimator;
 
 	void OnTriggerEnter2D(Collider2D other)
 	{
 		if (other.gameObject.tag.Equals ("Enemy")) {
 			//other.gameObject.GetComponent<EnemyScript> ().takeHit ();
-			AwakeGameOverWidget.SetActive(true);
+			//AwakeGameOverWidget.SetActive(true);
+			gameOverAnimator.SetTrigger("GameOver");
 		}
 	}
 }
