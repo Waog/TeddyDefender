da6155c604eac5d8e0a959d3be19dbf1c6827ffc
Oliver Stadie <hello@waog.net>
2016-03-02 17:45:26 +0100

    UPDATE #8: integrated small monster (ghost)

--- Assets/Scripts/Enemy/SquishyMonsterScript.cs
+++ Assets/Scripts/Enemy/SquishyMonsterScript.cs
@@ -5,7 +5,8 @@ public class SquishyMonsterScript : EnemyScript
 {
 	public GameObject explosion;
 
-	private EnemyVariablesScript vars;
+	EnemyVariablesScript vars;
+	bool isDying = false;
 
 	void Start() {
 		vars = GetComponent<EnemyVariablesScript> ();
@@ -13,13 +14,24 @@ public class SquishyMonsterScript : EnemyScript
 
 	void FixedUpdate()
 	{
+		if (isDying) {
+			return;
+		}
 		float z = Mathf.Atan2((vars.bed.transform.position.y - transform.position.y), (vars.bed.transform.position.x - transform.position.x)) * Mathf.Rad2Deg - 90;
 		transform.eulerAngles = new Vector3(0, 0, z);
 		GetComponent<Rigidbody2D>().AddForce(transform.up * vars.speed * GetComponent<Rigidbody2D>().mass);
 	}
 
 	public override void takeHit() {
-			Destroy (gameObject);
-			Instantiate (explosion, transform.position, Quaternion.identity);
+//		Instantiate (explosion, transform.position, Quaternion.identity);
+		GetComponent<CircleCollider2D> ().enabled = false;
+		GetComponentInChildren<ParticleSystem> ().Stop ();
+		transform.FindChild ("Red Eyes").gameObject.SetActive (false);
+		Invoke ("destroy", GetComponentInChildren<ParticleSystem> ().startLifetime);
+		isDying = true;
+	}
+
+	private void destroy() {
+		Destroy(gameObject);
 	}
 }
