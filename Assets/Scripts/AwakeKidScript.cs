c7e9adfce203a4aa9dec867a75edfdac0641a81a
Oliver Stadie <hello@waog.net>
2016-03-22 13:07:15 +0100

    FIXED #12 Kind wacht auf
    integrated sleepmeter

--- Assets/Scripts/AwakeKidScript.cs
+++ Assets/Scripts/AwakeKidScript.cs
@@ -1,18 +1,32 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class AwakeKidScript : MonoBehaviour {
 
 	public Animator gameOverAnimator;
 	public SceneTimer sceneTimer;
+	public Slider sleepmeter;
+	public float damagePerSecondPerMonster = 0.1f;
+	public float regenerationPerSecond = 0.01f;
 
-	void OnTriggerEnter2D(Collider2D other)
+	void OnTriggerStay2D(Collider2D other)
 	{
 		if (other.gameObject.tag.Equals ("Enemy")) {
 			//other.gameObject.GetComponent<EnemyScript> ().takeHit ();
 			//AwakeGameOverWidget.SetActive(true);
-			gameOverAnimator.SetTrigger("GameOver");
-			sceneTimer.freezeTimer ();
+
+
+			sleepmeter.value -= damagePerSecondPerMonster * Time.deltaTime;
+
+			if (sleepmeter.value <= 0) {
+				gameOverAnimator.SetTrigger("GameOver");
+				sceneTimer.freezeTimer ();
+			}
 		}
+	}
+
+	void Update() {
+		sleepmeter.value += regenerationPerSecond * Time.deltaTime;
 	}
 }
